cbac0a32ea3170f84737965e208f169649caf603
Adam Ratzman <adam@adamratzman.com>
2020-12-07 00:48:23 -0500

    cleaned up day 7

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -4,38 +4,20 @@ fun main() {
     val bags = input.split("\n").map { line ->
         val bag = line.split(" bags contain")[0]
         val others = if (line.contains("contain no other bags")) listOf()
-            else
-                line.split("bags contain ")[1].removeSuffix(".")
-            .split(", ")
-            .map { bagS ->
-                val num = bagS.split(" ")[0].toInt()
-                val name = bagS.split(" ").subList(1, bagS.split(" ").size - 1)
-                num to name.joinToString(" ")
-            }
+        else line.split("bags contain ")[1].removeSuffix(".").split(", ").map {
+            it.split(" ")[0].toInt() to it.split(" ").subList(1, it.split(" ").size - 1).joinToString(" ")
+        }
         bag to others
     }
 
-    val colors = mutableSetOf<String>()
-    fun recursePart1(startingBag: String, currentBag: String = startingBag) {
-        if (currentBag == "shiny gold") colors.add(startingBag)
-        else {
-            bags.first { it.first == currentBag }.second.forEach { bagsToGoTo ->
-                recursePart1(startingBag, bagsToGoTo.second)
-            }
-        }
-    }
+    fun recursePart1(startingBag: String, currentBag: String = startingBag): List<String> =
+        if (currentBag == "shiny gold") listOf(startingBag)
+        else bags.first { it.first == currentBag }.second.map { recursePart1(startingBag, it.second) }.flatten()
+    println("Part 1: ${bags.map { recursePart1(it.first) }.flatten().toSet().size - 1}")
 
-    bags.forEach { recursePart1(it.first) }
-    println("Part 1: ${colors.size - 1}")
-
-    fun recursePart2(startingBag: String, currentBag: String = startingBag): Int {
-        if (bags.first { it.first == currentBag }.second.isEmpty()) return 1
-
-        return 1 + bags.first { it.first == currentBag }.second.sumBy { bagsToGoTo ->
-            bagsToGoTo.first * recursePart2(startingBag, bagsToGoTo.second)
-        }
-    }
-
+    fun recursePart2(startingBag: String, currentBag: String = startingBag): Int =
+        if (bags.first { it.first == currentBag }.second.isEmpty()) 1
+        else 1 + bags.first { it.first == currentBag }.second.sumBy { it.first * recursePart2(startingBag, it.second) }
     println("Part 2: ${recursePart2("shiny gold") - 1}")
 }
 
